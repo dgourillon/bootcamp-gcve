e54967aa9993bbba05fd6bdfadab47e4a032cc8a
Didier Gourillon <dgourillon@google.com>
2023-08-21 17:32:50 +0200

    add hcx connect

--- hcx/hcx.ps1
+++ hcx/hcx.ps1
@@ -1,3 +1,8 @@
 Install-Module -Name VMware.PowerCLI -Scope CurrentUser -Force
 Connect-VIServer -Server VCENTER_URL -User 'CloudOwner@gve.local' -Password 'VCENTER_PWD' -Force
 Get-VM | Select Id
+
+	
+Get-Command -Module VMware.VimAutomation.HCX
+
+Connect-HCXServer -Server HCX_URL -User 'CloudOwner@gve.local' -Password VCENTER_PWD
