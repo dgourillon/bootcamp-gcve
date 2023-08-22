8961dcbe3ba411982c37e40e6c149f49fc2e7aab
Didier Gourillon <dgourillon@google.com>
2023-08-22 10:39:51 +0200

    add pair with remote site

--- hcx/hcx.ps1
+++ hcx/hcx.ps1
@@ -1,8 +1,12 @@
 Install-Module -Name VMware.PowerCLI -Scope CurrentUser -Force
-Connect-VIServer -Server VCENTER_URL -User 'CloudOwner@gve.local' -Password 'VCENTER_PWD' -Force
+Connect-VIServer -Server VCENTER_URL_PC1 -User 'CloudOwner@gve.local' -Password 'VCENTER_PWD_PC1' -Force
 Get-VM | Select Id
 
 	
 Get-Command -Module VMware.VimAutomation.HCX
 
-Connect-HCXServer -Server HCX_URL -User 'CloudOwner@gve.local' -Password VCENTER_PWD
+Connect-HCXServer -Server HCX_URL_PC1 -User 'CloudOwner@gve.local' -Password VCENTER_PWD_PC1
+
+$SecureString = ConvertTo-SecureString -String "VCENTER_PWD_PC2" -AsPlainText -Force
+
+New-HCXSitePairing -Password $SecureString -Url HCX_URL_PC2 -Username  'CloudOwner@gve.local'
--- hcx/set_credentials.sh
+++ hcx/set_credentials.sh
@@ -13,6 +13,22 @@ VCENTER_PWD=$(gcloud vmware private-clouds vcenter credentials describe --privat
 
 HCX_URL=$(gcloud vmware private-clouds list --location=$ZONE --format="value(hcx.fqdn)" --filter="name=projects/$PROJECT/locations/$ZONE/privateClouds/$PRIVATE_CLOUD" --project $PROJECT )
 
-sed -i  "s/VCENTER_URL/$VCENTER_URL/g" hcx.ps1
-sed -i  "s/VCENTER_PWD/$VCENTER_PWD/g" hcx.ps1
-sed -i  "s/HCX_URL/$HCX_URL/g" hcx.ps1
+sed -i  "s/VCENTER_URL_PC1/$VCENTER_URL/g" hcx.ps1
+sed -i  "s/VCENTER_PWD_PC1/$VCENTER_PWD/g" hcx.ps1
+sed -i  "s/HCX_URL_PC1/$HCX_URL/g" hcx.ps1
+
+
+PRIVATE_CLOUD="pc-2"
+ZONE="us-west2-a"
+PROJECT="gcve-dgo"
+
+VCENTER_IP=$(gcloud vmware private-clouds list --location=$ZONE --format="value(vcenter.internalIp)" --filter="name=projects/$PROJECT/locations/$ZONE/privateClouds/$PRIVATE_CLOUD" --project $PROJECT ) 
+VCENTER_URL=$(gcloud vmware private-clouds list --location=$ZONE --format="value(vcenter.fqdn)" --filter="name=projects/$PROJECT/locations/$ZONE/privateClouds/$PRIVATE_CLOUD" --project $PROJECT ) 
+VCENTER_USER=$(gcloud vmware private-clouds vcenter credentials describe --private-cloud=$PRIVATE_CLOUD --location=$ZONE --project $PROJECT --format="value(username)")
+VCENTER_PWD=$(gcloud vmware private-clouds vcenter credentials describe --private-cloud=$PRIVATE_CLOUD --location=$ZONE --project $PROJECT --format="value(password)")
+
+HCX_URL=$(gcloud vmware private-clouds list --location=$ZONE --format="value(hcx.fqdn)" --filter="name=projects/$PROJECT/locations/$ZONE/privateClouds/$PRIVATE_CLOUD" --project $PROJECT )
+
+sed -i  "s/VCENTER_URL_PC2/$VCENTER_URL/g" hcx.ps1
+sed -i  "s/VCENTER_PWD_PC2/$VCENTER_PWD/g" hcx.ps1
+sed -i  "s/HCX_URL_PC2/$HCX_URL/g" hcx.ps1
