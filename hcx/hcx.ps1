4696a68491a66b941308da886c2209e80312c0c8
Didier Gourillon <dgourillon@google.com>
2023-08-29 10:05:57 +0200

    add the service mesh creation

--- hcx/hcx.ps1
+++ hcx/hcx.ps1
@@ -10,3 +10,18 @@ Connect-HCXServer -Server HCX_URL_PC1 -User 'CloudOwner@gve.local' -Password VCE
 $SecureString = ConvertTo-SecureString -String "VCENTER_PWD_PC2" -AsPlainText -Force
 
 New-HCXSitePairing -Password $SecureString -Url HCX_URL_PC2 -Username  'CloudOwner@gve.local'
+
+$hcxServiceMeshName = "HCX-ServiceMesh"
+$hcxComputeProfileName = "GCVE Compute Profile"
+$hcxManagementNetworkProfileName = "GCVE MGMT Network Profile"
+$hcxManagementNetworkBackingName = "HCX-MGMT"
+$hcxComputeClusterName = "my-management-cluster"
+$hcxDatastoreName = "vsanDatastore"
+$hcxVDSName = "Datacenter-dvs"
+
+New-HCXServiceMesh -Name $hcxServiceMeshName  `
+-SourceComputeProfile $hcxLocalComputeProfile  `
+-Destination $HcxDstSite  `
+-DestinationComputeProfile $hcxRemoteComputeProfile  `
+-Service BulkMigration,Interconnect,Vmotion,WANOptimization,NetworkExtension  `
+-SourceUplinkNetworkProfile $hcxMgmtNetworkProfile
