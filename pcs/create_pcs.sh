fd2875fe48e12e3ee244e5401f5dc92228311e5d
Didier Gourillon <dgourillon@google.com>
2023-09-29 11:49:21 +0200

    add a cloud build yaml file

--- pcs/create_pcs.sh
+++ pcs/create_pcs.sh
@@ -2,7 +2,7 @@ gcloud vmware private-clouds create pc-1 \
 --location=us-central1-a \
 --project=gcve-dgo \
 --type=TIME_LIMITED \
---cluster=my-management-cluster \
+--cluster=nprd-ll01 \
 --node-type-config=type=standard-72,count=1 \
 --management-range=10.170.0.0/20 \
 --vmware-engine-network=us-central1-default
