0e29ddc9e4469fae1598d5f171aafe4d07ee0358
Didier Gourillon <dgourillon@google.com>
2023-12-14 17:14:58 +0100

    variabilize the shell scripts

--- pc-1/set_credentials.sh
+++ pc-1/set_credentials.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PRIVATE_CLOUD="pc-1"
-ZONE="us-central1-a"
-PROJECT="gcve-dgo"
+PRIVATE_CLOUD=$1
+ZONE=$2
+PROJECT=$3
 
 CREDENTIALS_TFVARS="gcve.credentials.auto.tfvars"
 
--- pcs/create_pcs.sh
+++ pcs/create_pcs.sh
@@ -1,49 +1,55 @@
-
+PROJECT=$1
+PC1_NAME=$2
+PC1_LOCATION=$3
+PC1_ADMIN_RANGE=$4
+PC2_NAME=$5
+PC2_LOCATION=$6
+PC2_ADMIN_RANGE=$7
 BUILD_PC_2=true
 
-if gcloud vmware private-clouds list --location us-central1-a | grep pc-1
+if gcloud vmware private-clouds list --location $PC1_LOCATION-a --project=$PROJECT | grep $PC1_NAME
 then
-   echo "pc-1 found, skip the build" 
+   echo "$PC1_NAME found, skip the build" 
 
 else
-    echo "pc-1 not found, trigger a build"
-    gcloud vmware private-clouds create pc-1 \
-    --location=us-central1-a \
-    --project=gcve-dgo \
+    echo "$PC1_NAME not found, trigger a build"
+    gcloud vmware private-clouds create $PC1_NAME \
+    --location=$PC1_LOCATION-a \
+    --project=$PROJECT \
     --type=TIME_LIMITED \
-    --cluster=nprd-ll01 \
+    --cluster=my-management-cluster \
     --node-type-config=type=standard-72,count=1 \
-    --management-range=10.170.0.0/20 \
-    --vmware-engine-network=us-central1-default
+    --management-range=$PC1_ADMIN_RANGE \
+    --vmware-engine-network=$PC1_LOCATION-default
     gsutil rm gs://tfstate-gcve-bootcamp/pc1/*
 fi
 
-if gcloud vmware private-clouds list --location us-west2-a | grep pc-2
+if gcloud vmware private-clouds list --location $PC2_LOCATION-a --project=$PROJECT | grep $PC2_NAME
 then
-   echo "pc-2 found, skip the build" 
+   echo "$PC2_NAME found, skip the build" 
 
 else
     if $BUILD_PC_2
     then
-        gcloud vmware private-clouds create pc-2 \
-        --location=us-west2-a \
-        --project=gcve-dgo \
+        gcloud vmware private-clouds create $PC2_NAME \
+        --location=$PC2_LOCATION-a \
+        --project=$PROJECT \
         --type=TIME_LIMITED \
         --cluster=my-management-cluster \
         --node-type-config=type=standard-72,count=1 \
         --management-range=10.170.16.0/20 \
-        --vmware-engine-network=us-west2-default
+        --vmware-engine-network=$PC2_LOCATION-default
         gsutil rm gs://tfstate-gcve-bootcamp/pc2/*
     fi
 fi
 
-until gcloud vmware private-clouds list --location us-central1-a --format="value(vcenter.state)" --filter="name:pc-1" | grep -i "active" 
+until gcloud vmware private-clouds list --location $PC1_LOCATION-a --format="value(vcenter.state)" --filter="name:$PC1_NAME" --project=$PROJECT | grep -i "active" 
 do 
     echo "waiting for the PC pc1 to finish building"
     sleep 300
 done
 
-until gcloud vmware private-clouds list --location us-west2-a --format="value(vcenter.state)" --filter="name:pc-2" | grep -i "active" 
+until gcloud vmware private-clouds list --location $PC2_LOCATION-a --format="value(vcenter.state)" --filter="name:$PC2_NAME" --project=$PROJECT | grep -i "active" 
 do 
     echo "waiting for the PC pc2 to finish building"
     sleep 300
