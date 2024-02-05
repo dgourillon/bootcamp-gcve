11ac5d674a0992d39d7f793c712b133999dcbc2e
Didier Gourillon <dgourillon@google.com>
2024-02-05 17:35:46 +0100

    fix

--- 10-destroy.sh
+++ 10-destroy.sh
@@ -3,8 +3,8 @@
 
 . ./00-variables.sh
 
-echo gcloud vmware private-clouds delete $PC2_NAME --project=$PROJECT_ID --location=$PC2_LOCATION
-gcloud vmware private-clouds delete $PC1_NAME --project=$PROJECT_ID --location=$PC1_LOCATION
+echo gcloud vmware private-clouds delete $PC2_NAME --project=$PROJECT_ID --location=$PC2_LOCATION-a
+gcloud vmware private-clouds delete $PC1_NAME --project=$PROJECT_ID --location=$PC1_LOCATION-a
 gcloud vmware network-peerings delete peering-psa --project=$PROJECT_ID 
 gcloud vmware network-peerings delete peering-vpc --project=$PROJECT_ID 
 gcloud vmware networks delete $GCVE_NETWORK_NAME --project=$PROJECT_ID 
